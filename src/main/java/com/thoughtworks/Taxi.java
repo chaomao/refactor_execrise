224e82d8c09acd36d765a2dae182d462cab02423
mao chao <6233843@gmail.com>
2018-01-23 00:06:02 +0800

    second refactor example

--- src/main/java/com/thoughtworks/Taxi.java
+++ src/main/java/com/thoughtworks/Taxi.java
@@ -1,5 +1,25 @@
 package com.thoughtworks;
 
 public class Taxi {
+  private int city;
+  static final int TIER_ONE = 1;
+  static final int TIER_TWO = 2;
+  static final int TIER_Three = 3;
 
+  public Taxi(int city) {
+    this.city = city;
+  }
+
+  public int charge(int miles) {
+    switch (city) {
+      case TIER_ONE:
+        return miles * (miles >= 10 ? 50 : 30) + (miles >= 10 ? 100 : 0);
+      case TIER_TWO:
+        return miles * (miles >= 10 ? 25 : 20);
+      case TIER_Three:
+        return miles * 10;
+      default:
+        throw new RuntimeException("unsupport city");
+    }
+  }
 }
---
+++ test/com/thoughtworks/TaxiTest.java
@@ -0,0 +1,39 @@
+package com.thoughtworks;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+
+public class TaxiTest {
+  @Test
+  public void TierThreeChargeMileWith10() throws Exception {
+    Taxi taxi = new Taxi(Taxi.TIER_Three);
+    assertThat(taxi.charge(5), is(5 * 10));
+  }
+
+  @Test
+  public void TierTwoChargeMileWith20WhenMileLessThan10Miles() throws Exception {
+    Taxi taxi = new Taxi(Taxi.TIER_TWO);
+    assertThat(taxi.charge(9), is(9 * 20));
+  }
+
+  @Test
+  public void TierTwoChargeMileWith25WhenMileMOREThan10Miles() throws Exception {
+    Taxi taxi = new Taxi(Taxi.TIER_TWO);
+    assertThat(taxi.charge(10), is(10 * 25));
+  }
+
+  @Test
+  public void TierONEChargeMileWith30WhenMileLessThan10Miles() throws Exception {
+    Taxi taxi = new Taxi(Taxi.TIER_ONE);
+    assertThat(taxi.charge(9), is(9 * 30));
+  }
+
+  @Test
+  public void TierONEChargeMileWith50WAnd100TaxWhenMileMOREThan10Miles() throws Exception {
+    Taxi taxi = new Taxi(Taxi.TIER_ONE);
+    assertThat(taxi.charge(10), is(10 * 50 + 100));
+  }
+}
